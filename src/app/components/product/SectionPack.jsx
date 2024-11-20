de41aabe3c1d2f75d514e40482ffc33b12d0bc9e
YedixonRDev <yedixonjrf@gmail.com>
2024-11-20 00:48:47 -0500

    Se hicieron ajustes en la paginacion

--- src/app/components/product/SectionPack.jsx
+++ src/app/components/product/SectionPack.jsx
@@ -36,7 +36,7 @@ export default function SectionPack() {
   const nextRef = useRef(null);
 
   return (
-    <div className="max-w-2xl mx-auto p-6">
+    <div className="max-w-md mx-auto p-6">
       <h2 className="text-2xl font-bold text-center text-[#1f6c76] mb-6">
         Empaques Destacados
       </h2>
